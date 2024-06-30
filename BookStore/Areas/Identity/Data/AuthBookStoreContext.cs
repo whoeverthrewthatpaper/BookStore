3a2244f1de3609a0bcfd818bda3ac3daf0bb1df9
whoeverthrewthatpaper <75176559+whoeverthrewthatpaper@users.noreply.github.com>
2024-07-01 01:41:49 +0200

    Initial commit

---
+++ BookStore/Areas/Identity/Data/AuthBookStoreContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class AuthBookStoreContext : IdentityDbContext<BookStoreUser>
+    {
+        public AuthBookStoreContext(DbContextOptions<AuthBookStoreContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize
+        }
+    }
+}
---
+++ BookStore/Models/Repositories/BookRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repositories
+{
+    public class BookRepository : IBookStoreRepository<Book>
+    {
+        List<Book> Books; 
+        public BookRepository()
+        {
+            Books = new List<Book>() 
+            {
+                new Book
+                {
+                    Id = 1, 
+                    Title = "abcd", 
+                    Description = "gfejdoncsopmwes", 
+                    ImageUrl = "", //image
+                    Author = new Author{Id = 1, FullName = "auth1"}
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "rwcdsxa",
+                    Description = "gvjofnckwdmslpx,;zgvfjcdskl,xrfewweww",
+                    ImageUrl = "",//image
+                    Author = new Author{Id = 1, FullName = "auth2"}
+                },
+                new Book
+                {
+                    Id = 3,
+                    Title = "poiuy",
+                    Description = "ghbfniwdcskxlkvefdmc",
+                    ImageUrl = "",//images
+                    Author = new Author{Id = 1, FullName = "auth3"}
+                }
+            };
+        }
+        public void Add(Book NewBook)
+        {
+            NewBook.Id = Books.Max(b => b.Id) + 1; 
+            Books.Add(NewBook);
+        }
+
+        public void Delete(int Id)
+        {
+            Book book = Find(Id);
+            Books.Remove(book);
+        }
+
+        public Book Find(int Id)
+        {
+            Book book = Books.SingleOrDefault(b => b.Id == Id);
+            return book; 
+        }
+
+        public IList<Book> List()
+        {
+            return Books; 
+        }
+
+        public List<Book> Search(string term)
+        {
+            var result = Books.
+                Where(b =>
+                b.Title.Contains(term) ||
+                b.Description.Contains(term) ||
+                b.Author.FullName.Contains(term)
+                ).ToList();
+            return result;
+        }
+
+        public void Update(int Id, Book NewBook)
+        {
+            Book book = Find(Id);
+            book.Title = NewBook.Title;
+            book.Description = NewBook.Description;
+            book.Author = NewBook.Author;
+            book.ImageUrl = NewBook.ImageUrl;
+        }
+
+        public void Update(Book Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
